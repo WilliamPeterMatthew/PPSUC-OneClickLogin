50cee35af2c94e619c5ad43f2feaf8c76bbb9f92
ZhangKaiXin <zkx20020420@foxmail.com>
2025-01-02 11:03:25 +0800

    Add files via upload

--- PPSUC-OneClickLogin.py
+++ PPSUC-OneClickLogin.py
@@ -13,7 +13,8 @@ data={
     'R6':'0',
     'para':'00',
     'v6ip':'',
-    '_':str(randrange(1000000000000,9999999999999))}
+    '_':str(randrange(1000000000000,9999999999999))
+}
 header={
     'Accept':'*/*',
     'Accept-Encoding':'gzip, deflate',
@@ -22,7 +23,8 @@ header={
     'DNT':'1',
     'Host':'192.168.8.123',
     'Referer':'http://192.168.8.123/',
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'}
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'
+}
 url = 'http://192.168.8.123/drcom/login?callback=dr1730552179698&DDDDD='+quote(data['DDDDD'])+'&upass='+quote(data['upass'])+'&0MKKey=123456&R1=0&R3=0&R6=0&para=00&v6ip=&_=1370215579896'
 response = requests.post(url,data=data,headers=header)
 if response.status_code == 200:
